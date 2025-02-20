a0f8282da2ac48fa1ace0e5a325fe21511548ad9
Jepasq <jepasq111@gmail.com>
2025-02-20 10:26:36 +0100

    Add a very simple usage message to list example

--- src/examples/list.c
+++ src/examples/list.c
@@ -1,13 +1,39 @@
 #include "list.h"
 
-#include <stdio.h> // USES printf()
+#include <stdio.h>    // USES printf()
 #include <stdbool.h>  // USES bool
+#include <string.h>   // USES strcmp()
 
 // Created after a endless loop/segfault in list_flatten
 
 // Forward declarations
 void print_len(List*, bool);
 // End of forward declarations
+
+/// Return true if the given array contains a help-related command line flag
+bool
+has_help(char** argv)
+{
+  char* s = argv[1];
+  if (strcmp(s, "-?") ==0 ||
+      strcmp(s, "-h") ==0 ||
+      strcmp(s, "--help") ==0)
+    return true;
+
+  return false;
+}
+
+/// Always return 0 (success status code)
+int
+usage(char* progname)
+{
+  printf("Some test around the List type implementation\n");
+
+  printf("\nUsage :\n");
+  printf("  %s [-?|-h|-help] : shows this message and ", progname);
+  printf("exit with success status.\n");
+
+}
 
 /** List example main entry
   *
@@ -20,6 +46,10 @@ void print_len(List*, bool);
 int
 main(int argc, char** argv)
 {
+  if (argc > 1 && has_help(argv))
+      return usage(argv[0]);
+
+  
   List* lst = list_create("aa");
   print_len(lst, false);
   
@@ -33,7 +63,7 @@ main(int argc, char** argv)
 /** Print the lenght in elements of the given list to stdout
   *
   * @param lst The list to print print.
-  * @param now Shouls we add the 'now' word.
+  * @param now Should we add the 'now' word.
   *
   */
 void
